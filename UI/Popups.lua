fdde0b9f5822ed56d7b3cd39d168f1a6363da487
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-01-08 13:46:26 +0100

    Added customShortcutInstructions to Popups:OpenURL

--- UI/Popups.lua
+++ UI/Popups.lua
@@ -7,7 +7,9 @@ local format = string.format;
 -- WoW imports
 local _G = _G;
 
-local COPY_LINK = [[You can copy this link by using the %s keyboard shortcut and then paste the link inside your browser using the %s shortcut.]];
+local COPY_LINK = [[
+You can copy this link by using the %s keyboard shortcut and then paste the link inside your browser using the %s shortcut.
+]];
 
 ---@param Ellyb Ellyb @ Instance of the library
 local function OnLoad(Ellyb, env)
@@ -19,14 +21,20 @@ local function OnLoad(Ellyb, env)
 
 	---@type Frame
 	local URLPopup = _G.Ellyb_StaticPopOpenUrl;
+	URLPopup.Button.Text:SetText(_G.OKAY);
 
 	--- Open a popup with an autofocused text field to let the user copy the URL
 	---@param url string @ The URL we want to let the user copy
 	---@param customText string @ A custom text to display, instead of the default hint to copy the URL
-	function Popups:OpenURL(url, customText)
-		URLPopup.Text:SetText(customText or format(COPY_LINK, Ellyb.ColorManager.ORANGE(copyShortcut), Ellyb.ColorManager.ORANGE(pasteShortcut)));
+	---@param customShortcutInstructions string @ A custom text for the copy and paste shortcut instructions.
+	function Popups:OpenURL(url, customText, customShortcutInstructions)
+		local popupText = customText or "";
+		if not customShortcutInstructions then
+			customShortcutInstructions = COPY_LINK;
+		end
+		popupText = popupText .. format(customShortcutInstructions, Ellyb.Strings.COMMON_SHORTCUTS.COPY, Ellyb.Strings.COMMON_SHORTCUTS.PASTE);
+		URLPopup.Text:SetText(popupText);
 		URLPopup.Url:SetText(url);
-		URLPopup.Button.Text:SetText("Okay");
 		URLPopup:Show();
 	end
 
@@ -37,9 +45,6 @@ local function OnModulesLoad(Ellyb, env)
 	setfenv(1, env);
 
 	Logger = Ellyb.Logger("Popups");
-
-	copyShortcut = Ellyb.Strings.systemKeyboardShortcut("Ctrl", "C");
-	pasteShortcut = Ellyb.Strings.systemKeyboardShortcut("Ctrl", "V");
 end
 
 Ellyb.ModulesManagement:RegisterNewModule("Popups", OnLoad, OnModulesLoad);
