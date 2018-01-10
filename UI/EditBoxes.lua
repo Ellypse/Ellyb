2b5f790607e4d2159def2b6275d53c54ef1cbed5
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-01-10 14:01:26 +0100

    Added EditBoxes.setupTabKeyNavigation(...)

--- UI/EditBoxes.lua
+++ UI/EditBoxes.lua
@@ -1,10 +1,16 @@
 ---@type Ellyb
 local Ellyb = Ellyb:GetInstance(...);
 
+-- Lua imports
+local pairs = pairs;
+
+-- WoW imports
+local IsShiftKeyDown = IsShiftKeyDown;
+
 local EditBoxes = {};
 
 ---@param editBox EditBox|ScriptObject
-local function saveEditboxOriginalText(editBox)
+local function saveEditBoxOriginalText(editBox)
 	if editBox.readOnly then
 		editBox.originalText = editBox:GetText();
 	end
@@ -22,7 +28,7 @@ function EditBoxes.makeReadOnly(editBox)
 
 	editBox.readOnly = true;
 
-	editBox:HookScript("OnShow", saveEditboxOriginalText);
+	editBox:HookScript("OnShow", saveEditBoxOriginalText);
 
 	editBox:HookScript("OnTextChanged", restoreOriginalText);
 end
@@ -36,6 +42,34 @@ end
 ---@param editBox EditBox|ScriptObject
 function EditBoxes.looseFocusOnEscape(editBox)
 	editBox:HookScript("OnEscapePressed", editBox.ClearFocus);
+end
+
+---Setup keyboard navigation using the tab key inside a list of EditBoxes.
+---Pressing tab will jump to the next EditBox in the list, and shift-tab will go back to the previous one.
+---@param ... EditBox[] @ A list of EditBoxes
+function EditBoxes.setupTabKeyNavigation(...)
+	local editBoxes = { ... };
+	local maxBound = #editBoxes;
+	local minBound = 1;
+	for index, editbox in pairs(editBoxes) do
+		editbox:SetScript("OnTabPressed", function(self, button)
+			local cursor = index
+			if IsShiftKeyDown() then
+				if cursor == minBound then
+					cursor = maxBound
+				else
+					cursor = cursor - 1
+				end
+			else
+				if cursor == maxBound then
+					cursor = minBound
+				else
+					cursor = cursor + 1
+				end
+			end
+			editBoxes[cursor]:SetFocus();
+		end)
+	end
 end
 
 Ellyb.EditBoxes = EditBoxes;
