013310421a8367137959d05e3f69b5ffb248c333
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-01-19 14:46:33 +0100

    Fixed copy URL Popup height, keyboard, anchors

--- UI/Popups.lua
+++ UI/Popups.lua
@@ -24,17 +24,24 @@ EditBoxes.makeReadOnly(URLPopup.Url);
 EditBoxes.selectAllTextOnFocus(URLPopup.Url);
 EditBoxes.looseFocusOnEscape(URLPopup.Url);
 
+local function dismissPopup()
+	HideUIPanel(URLPopup);
+end
+URLPopup.Url:HookScript("OnEnterPressed", dismissPopup);
+URLPopup.Url:HookScript("OnEscapePressed", dismissPopup);
+
 --- Open a popup with an autofocused text field to let the user copy the URL
 ---@param url string @ The URL we want to let the user copy
 ---@param customText string @ A custom text to display, instead of the default hint to copy the URL
 ---@param customShortcutInstructions string @ A custom text for the copy and paste shortcut instructions.
 function Popups:OpenURL(url, customText, customShortcutInstructions)
-	local popupText = customText or "";
+	local popupText = customText and (customText .. "\n\n") or "";
 	if not customShortcutInstructions then
 		customShortcutInstructions = COPY_LINK;
 	end
 	popupText = popupText .. format(customShortcutInstructions, ORANGE(Ellyb.System.SHORTCUTS.COPY), ORANGE(Ellyb.System.SHORTCUTS.PASTE));
 	URLPopup.Text:SetText(popupText);
 	URLPopup.Url:SetText(url);
+	URLPopup:SetHeight(120 + URLPopup.Text:GetHeight());
 	URLPopup:Show();
 end
