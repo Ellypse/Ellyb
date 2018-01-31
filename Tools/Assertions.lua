5320fde528a5a32738652f6d3b4b5d3e82c3c610
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-01-31 13:55:36 +0100

    Added isInstanceOf class assertion

--- Tools/Assertions.lua
+++ Tools/Assertions.lua
@@ -21,6 +21,7 @@ local DEBUG_WRONG_WIDGET_TYPE = [[Invalid Widget type "%2$s" for variable "%1$s"
 local DEBUG_WRONG_VARIABLE_TYPES = [[Invalid variable type "%2$s" for variable "%1$s", expected one of (%3$s).]];
 local DEBUG_WRONG_WIDGET_TYPES = [[Invalid Widget type "%2$s" for variable "%1$s", expected one of (%3$s).]];
 local DEBUG_EMPTY_VARIABLE = [[Variable "%s" cannot be empty.]];
+local DEBUG_WRONG_CLASS = [[Invalid Class "%2$s" for variable "%1$s", expected "%3$s".]];
 
 ---Check if a variable is of the expected type ("number", "boolean", "string")
 ---Can also check for Widget type ("Frame", "Button", "Texture")
@@ -136,4 +137,28 @@ function Assertions.isNotEmpty(variable, variableName)
 	else
 		return true;
 	end
+end
+
+--- Check if a variable is an instance of a specified class, taking polymorphism into account, so inherited class will pass the test.
+---@param variable Object @ The object to test
+---@param class string @ The name of the expected class as a string
+---@param variableName string @ The name of the variable being tested, will be visible in the error message
+function Assertions.isInstanceOf(variable, class, variableName)
+	if not Ellyb:IsDebugModeEnabled() then
+		return true
+	end;
+	local variableType = type(variable);
+
+	if not variableType == "table" or not variable.isInstanceOf or not variable.class then
+		-- The variable is not a Class
+		return false, format(DEBUG_WRONG_CLASS, variableName, variableType, class);
+	end
+
+	-- Check if the variable is an instance of the given class (taking polymorphism into account)
+	if not variable:isInstanceOf(class) then
+		-- The variable is an instance of a different class
+		return false, format(DEBUG_WRONG_CLASS, variableName, variable.class);
+	end
+
+	return true;
 end
