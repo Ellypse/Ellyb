1bfa7e33bf86575a8b3b59526ba77f77f4b442b2
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-03-17 00:35:43 +0100

    New module Localization

--- Ellyb.lua
+++ Ellyb.lua
@@ -4,7 +4,7 @@ local AddOnName = ...;
 local pairs = pairs;
 local assert = assert;
 
-local VERSION_NUMBER = 1.4;
+local VERSION_NUMBER = 1.5;
 local DEBUG_MODE = true;
 local instances = {};
 local addonVersions = {};
