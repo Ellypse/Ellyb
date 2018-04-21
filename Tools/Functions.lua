518f0517329bd1f9d4c6ffe30a36dee45811e5fd
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-04-21 10:22:06 +0200

    Minor code documentation fixes

--- Tools/Functions.lua
+++ Tools/Functions.lua
@@ -7,17 +7,18 @@ end
 
 ---@class Functions
 local Functions = {};
-Ellyb.Functions = Functions;
 
 --- Binds a given function to a value, returning a closure will call the
---  original function with the given value as the first argument, and
---  all arguments to the closure after it.
---
---  This is super useful for cases where you want to invoke an object method
---  but are being asked for a naked callback.
---
---  @param fn The function to wrap.
---  @param value The value to be supplied to fn as the first argument.
+--- original function with the given value as the first argument, and
+--- all arguments to the closure after it.
+---
+--- This is super useful for cases where you want to invoke an object method
+--- but are being asked for a naked callback.
+---
+--- @param fn function @ The function to wrap.
+--- @param value any @ The value to be supplied to fn as the first argument.
 function Functions.bind(fn, value)
 	return function(...) return fn(value, ...); end
 end
+
+Ellyb.Functions = Functions;
