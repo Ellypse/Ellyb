b2f28392ebd39bdc0fa0a62f4e1a400d93f9a0b7
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-06-16 15:11:39 +0200

    GameEvents uses EventsDispatcher

--- Tools/GameEvents.lua
+++ Tools/GameEvents.lua
@@ -20,82 +20,47 @@ local Logger = Ellyb.Logger("GameEvents");
 local GameEvents = {};
 Ellyb.GameEvents = GameEvents;
 
-local REGISTERED_EVENTS = {};
+local eventsDispatcher = Ellyb.EventsDispatcher();
 
-local REGISTERED_CALLBACK = "Registered a new event callback for event %s with id %s.";
-local UNREGISTERED_CALLBACK = "Unregistered an event callback for event %s with id %s.";
-local EVENT_HANDLER_NOT_FOUND = "handlerID not found %s.";
-local REGISTERED_EVENT = "Listening to new event %s.";
-local UNREGISTERED_EVENT = "Stopped listening to event %s, no more callbacks for this event.";
+local REGISTERED_EVENT = "Listening to new Game event %s.";
+local UNREGISTERED_EVENT = "Stopped listening to Game event %s, no more callbacks for this event.";
 
 local EventFrame = CreateFrame("FRAME");
 
 ---Register a callback for a game event
 ---@param event string @ A game event to listen to
 ---@param callback func @ A callback that will be called when the event is fired with its arguments
-function GameEvents.registerHandler(event, callback)
+function GameEvents.registerCallback(event, callback)
 	assert(Ellyb.Assertions.isType(event, "string", "event"));
 	assert(Ellyb.Assertions.isType(callback, "function", "callback"));
 
-	if not REGISTERED_EVENTS[event] then
+	if not EventFrame:IsEventRegistered(event) then
 		EventFrame:RegisterEvent(event);
-		REGISTERED_EVENTS[event] = {};
 		Logger:Info(format(REGISTERED_EVENT, tostring(event)))
 	end
 
-	local handlerID = Ellyb.Strings.generateUniqueID(REGISTERED_EVENTS);
-	REGISTERED_EVENTS[event][handlerID] = callback;
-
-	Logger:Info(format(REGISTERED_CALLBACK, tostring(event), handlerID));
-
-	return handlerID;
+	return eventsDispatcher:RegisterCallback(event, callback);
 end
 
 ---Unregister a previously registered callback using the handler ID given at registration
 ---@param handlerID string @ The handler ID of a previsouly registered callback that we want to unregister
-function GameEvents.unregisterHandler(handlerID)
+function GameEvents.unregisterCallback(handlerID)
 	assert(Ellyb.Assertions.isType(handlerID, "string", "handlerID"));
 
-	-- Look for the handler ID through all the events
-	for event, eventTab in pairs(REGISTERED_EVENTS) do
-		if eventTab[handlerID] then
-			eventTab[handlerID] = nil;
-
-			-- If this specific event no longer has any callback, we can remove it and unregister it
-			if Ellyb.Tables.size(eventTab) == 0 then
-				REGISTERED_EVENTS[event] = nil;
-				EventFrame:UnregisterEvent(event);
-				Logger:Info(format(UNREGISTERED_EVENT, tostring(event)));
-			end
-
-			Logger:Info(format(UNREGISTERED_CALLBACK, tostring(event), handlerID));
-
-			return;
-		end
-	end
-	Logger:Warning(format(EVENT_HANDLER_NOT_FOUND, handlerID));
+	eventsDispatcher:UnregisterCallback(handlerID);
 end
 
-local function eventDispatcher(self, event, ...)
-	-- Callbacks
-	if REGISTERED_EVENTS[event] then
-		local temp = Ellyb.Tables.getTempTable();
-
-		-- We use a separate structure as the callback could change REGISTERED_EVENTS[event]
-		Ellyb.Tables.copy(temp, REGISTERED_EVENTS[event])
-		for _, callback in pairs(temp) do
-			callback(...);
-		end
-
-		Ellyb.Tables.releaseTempTable(temp);
+local function dispatchEvent(self, event, ...)
+	if eventsDispatcher:HasCallbacksForEvent(event) then
+		eventsDispatcher:TriggerEvent(event, ...);
 	else
 		self:UnregisterEvent(event);
 		Logger:Info(format(UNREGISTERED_EVENT, tostring(event)));
 	end
 end
 
-EventFrame:SetScript("OnEvent", eventDispatcher);
+EventFrame:SetScript("OnEvent", dispatchEvent);
 
 function GameEvents.fireEvent(event, ...)
-	eventDispatcher(EventFrame, event, ...)
+	dispatchEvent(EventFrame, event, ...)
 end
