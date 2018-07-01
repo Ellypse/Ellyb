154fa2801d11b29094dc3947832d415309cfc4e1
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-07-01 11:25:29 +0200

    Added new DeprecationWarnings methods

--- Tools/DeprecationWarnings.lua
+++ Tools/DeprecationWarnings.lua
@@ -5,12 +5,21 @@ if Ellyb.DeprecationWarnings then
 	return
 end
 
+-- Lua imports
+local setmetatable = setmetatable;
+local debugstack = debugstack;
+
 local DeprecationWarnings = {};
 
 local logger = Ellyb.Logger("Deprecation warnings");
 
 local DEPRECATED_API_WARNING = [[DEPRECATED USAGE OF API %s.
 Please use %s instead.
+Stack: %s]];
+local DEPRECATED_FUNCTION_WARNING = [[DEPRECATED USAGE OF FUNCTION %s.
+Please use %s instead.
+Stack: %s]];
+local DEPRECATED_CUSTOM_WARNING = [[%s
 Stack: %s]];
 
 function DeprecationWarnings.wrapAPI(newAPITable, oldAPIName, newAPIName)
@@ -24,6 +33,17 @@ function DeprecationWarnings.wrapAPI(newAPITable, oldAPIName, newAPIName)
 			newAPITable[key] = value;
 		end
 	})
+end
+
+function DeprecationWarnings.wrapFunction(newFunction, oldFunctionName, newFunctionName)
+	return function(...)
+		logger:Warning(DEPRECATED_FUNCTION_WARNING:format(oldFunctionName, newFunctionName, debugstack(2, 3, 0)));
+		return newFunction(...);
+	end
+end
+
+function DeprecationWarnings.warn(customWarning)
+	logger:Warning(DEPRECATED_CUSTOM_WARNING:format(customWarning, debugstack(2, 3, 0)));
 end
 
 Ellyb.DeprecationWarnings = DeprecationWarnings;
