c4585651e332717057f0039c5ce5425d65144793
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-07-15 16:27:37 +0200

    Incremented version number

--- Ellyb.lua
+++ Ellyb.lua
@@ -4,7 +4,7 @@ local AddOnName = ...;
 local pairs = pairs;
 -- local assert = assert;
 
-local VERSION_NUMBER = 2.2;
+local VERSION_NUMBER = 2.3;
 local DEBUG_MODE = true;
 local instances = {};
 local addonVersions = {};
