3f816195b94dc900dadf3de5d14cfc46fa39da9f
Ellypse <ellypse@totalrp3.info>
2019-03-03 22:33:08 +0100

    Update Ellyb.lua
    Updated version number

--- Ellyb.lua
+++ Ellyb.lua
@@ -4,7 +4,7 @@ local AddOnName = ...;
 local pairs = pairs;
 -- local assert = assert;
 
-local VERSION_NUMBER = 8;
+local VERSION_NUMBER = 9;
 local DEBUG_MODE = true;
 local instances = {};
 local addonVersions = {};
